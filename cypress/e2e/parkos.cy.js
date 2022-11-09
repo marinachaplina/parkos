86f0253aa96512356041eac681fd9003873b59c5
marina <marinachaplina@gmail.com>
2022-11-09 13:50:49 +0100

    data via url

--- cypress/e2e/parkos.cy.js
+++ cypress/e2e/parkos.cy.js
@@ -5,17 +5,19 @@ describe('Parkos reservations', () => {
   
   const Reserv = new ReservationDetailsPage();
   const Home = new HomePage();
-
   const start = new Date('November 28, 2022 06:00:00');
   const end = new Date('November 29, 2022 20:00:00');
 
   it('passes', () => {
 
-    // HomePage - Select Airport, Date and Time
     cy.viewport(1920, 1080);
-    cy.visit('https://parkos.com/');
     
-    Home.choosePeriod('Las Vegas', start, end);
+    // OPTION 1 - Open the Home Page - Select Airport, Date and Time
+    // cy.visit('https://parkos.com/');
+    // Home.choosePeriod('Las Vegas', start, end);
+    
+    // OPTION 2 - Skip the Home Page
+    cy.visit('https://parkos.com/las-vegas-airport-parking/search/?location=las-vegas-airport-parking&arrivalTime=6%3A00&departureTime=20%3A00&arrival=2022-11-28&departure=2022-11-29&sort_f=price&sort_w=asc&version=5');
 
     // Page2 - Available parking places
     cy.get('.text-base').should('not.include.text', '0 available');
