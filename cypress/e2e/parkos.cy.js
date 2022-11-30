d92749e8a02ba5828fc3541d794368aa136273ce
marina <marinachaplina@gmail.com>
2022-11-30 16:54:02 +0100

    date-and-time library, fixture (user info)

--- cypress/e2e/parkos.cy.js
+++ cypress/e2e/parkos.cy.js
@@ -1,5 +1,6 @@
 import ReservationDetailsPage from '../support/Pages/ReservationDetailsPage';
 import HomePage from '../support/Pages/HomePage';
+import date from 'date-and-time';
 
 describe('Parkos reservations', () => {
   
@@ -9,15 +10,35 @@ describe('Parkos reservations', () => {
   const airport = 'Las Vegas';
   const airportUrl = 'las-vegas-airport-parking';
   let startDate;
+  let PeriodDays = 7;
+  let PeriodHours = 2;
   let endDate;
+  let user;
+
+  // Set startDate = now + 14 days and round minutes
+  const now = new Date();
+  startDate = date.addDays(now, +14);
   
-  //Use the cy.fixture() method to pull data from fixture file
+  let minutes = startDate.getMinutes();
+  if (minutes < 15) {
+    startDate.setMinutes(0);
+  } else if (minutes < 30) {
+    startDate.setMinutes(15);
+  } else if (minutes < 45) {
+    startDate.setMinutes(30);
+  } else {
+    startDate.setMinutes(45);
+  }
+
+  // Set parking period
+  endDate = structuredClone(startDate);
+  endDate.setDate(startDate.getDate() + PeriodDays);
+  endDate.setHours(startDate.getHours() + PeriodHours);
+
+  // Use the cy.fixture() method to pull User Info from fixture file
   before(function () {
-    cy.fixture('jsonData').then(function(data) {
-      startDate = new Date(data.StartDate);
-      endDate = structuredClone(startDate);
-      endDate.setDate(startDate.getDate() + data.PeriodDays);
-      endDate.setHours(startDate.getHours() + data.PeriodHours);
+    cy.fixture('userInfo').then(function (userInfo) {
+      user = userInfo;      
     })
   })
 
@@ -32,14 +53,11 @@ describe('Parkos reservations', () => {
     // OPTION 2 - Enter data via URL
     cy.visit('https://parkos.com/' + airportUrl + '/search/?location=' + airportUrl 
       + '&arrivalTime=' + startDate.getHours() + '%3A' + startDate.toLocaleString('en-US', {'hour': '2-digit', 'minute':'2-digit', hour12:false}).split(':')[1] 
-      + '&departureTime=' + endDate.getHours() + '%3A' + endDate.toLocaleString('en-US', {'second': '2-digit', 'minute':'2-digit'}).split(':')[0] 
+      + '&departureTime=' + endDate.getHours() + '%3A' + endDate.toLocaleString('en-US', {'hour': '2-digit', 'minute':'2-digit', hour12:false}).split(':')[1] 
       + '&arrival=' + startDate.getFullYear() + '-' + startDate.toLocaleString('en-US', {'month':'2-digit'}) + '-' + startDate.toLocaleString('en-US', {'day': '2-digit'})
       + '&departure=' + endDate.getFullYear() + '-' + endDate.toLocaleString('en-US', {'month':'2-digit'}) + '-' + endDate.toLocaleString('en-US', {'day': '2-digit'}) 
       + '&sort_f=price&sort_w=asc&version=5');
     
-      console.log(startDate.toLocaleString('en-US', {'minute':'2-digit'}));
-      console.log(startDate.toLocaleString('en-US', {'hour': '2-digit', 'minute':'2-digit', hour12:false}).split(':')[1]);
-
     // Page2 - Available parking places
     cy.wait(2000);
     cy.get('.text-base').should('not.include.text', '0 available');
@@ -48,11 +66,12 @@ describe('Parkos reservations', () => {
     // Page3 - Fill the Reservation details
     cy.get('h3').should('include.text', 'Your travel details');
     cy.wait(2000);
-    Reserv.fillForm('Fl345', 'Fl346', 'Marina', 'Marina@test.com', 'Marina@test.com', '12345678', '2', 'Volkswagen Golf7', 'KL1234H', 'base', 'Visa');
+    
+    Reserv.fillForm(user.DepartureFlightnr, user.ReturnFlightnr, user.Name, user.Email, user.Email2, user.Phone, user.Persons, user.Car, user.Sign, user.Package, user.Payment);
 
     Reserv.clickAgree();
     Reserv.clickChecked();
     
-    // cy.get('#reservationSubmit').click()
+    //cy.get('#reservationSubmit').click()
   })
 });
